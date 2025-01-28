d809d9f17c3f535cd8c31511b1e51b0c9871089a
xbze3 <ezranminty@gmail.com>
2025-01-27 23:04:36 -0400

    Additions
    init.js
    mintflow-db.decks.json
    mintflow-db.cards.json

--- db/init.js
+++ db/init.js
@@ -1,3 +1,16 @@
 db = db.getSiblingDB("mintflow-db");
 db.createCollection("cards");
 db.createCollection("decks");
+
+const cardsData = JSON.parse(
+    cat("/docker-entrypoint-initdb.d/mintflow-db.cards.json")
+);
+const decksData = JSON.parse(
+    cat("/docker-entrypoint-initdb.d/mintflow-db.decks.json")
+);
+
+db.myCollection1.insertMany(collection1Data);
+db.myCollection2.insertMany(collection2Data);
+
+db.decks.insertMany();
+db.cards.insertMany();
