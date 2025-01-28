7b2ee2b70b086fdf8c9d091641b19bdcb314b904
xbze3 <ezranminty@gmail.com>
2025-01-27 23:16:00 -0400

    Update init.js

--- db/init.js
+++ db/init.js
@@ -6,11 +6,8 @@ const cardsData = JSON.parse(
     cat("/docker-entrypoint-initdb.d/mintflow-db.cards.json")
 );
 const decksData = JSON.parse(
-    cat("/docker-entrypoint-initdb.d/mintflow-db.decks.json")
+    cat("/docker-entrypoint-initdb.d/mintflow-db.cards.json")
 );
 
-db.myCollection1.insertMany(collection1Data);
-db.myCollection2.insertMany(collection2Data);
-
-db.decks.insertMany();
-db.cards.insertMany();
+db.cards.insertMany(cardsData);
+db.decks.insertMany(decksData);
